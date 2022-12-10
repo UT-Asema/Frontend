91d40a7bb178403896e3614bc531a0c3b198df2e
Antonio Rochnean <antor@Antonios-MacBook-Air.local>
2022-12-10 21:22:27 +0200

    (Create 10.12.2022 21:21) Bottom, Middle, Homepage.css. (Update 10.12.2022 21:21) Homepage.jsx SignUp App index

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Top from "./Parts/Top";
 import Bottom from "./Parts/Bottom";
 import Middle from "./Parts/Middle";
+import "./Homepage.css"
 
-const Homepage = () => {
+function Homepage () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+      else {
+        entry.target.classList.remove('show');
+      }
+    })
+  })
+  useEffect(() => {
+    const hiddenElenents = document.querySelectorAll('.hidel');
+    hiddenElenents.forEach((el) => observer.observe(el));
+  }, [])
   return (
-    <div>
-      <Top />
-      <Middle />
-      <Bottom />
+    <div className="overflow-auto grid items-center mb-12">
+      <div className="hideel grid items-center min-h-screen text-center text-2xl overflow-auto">
+        <section className="hidel m-0 bg-[#ba45b4]">
+          <Top />
+        </section>
+        <section className="hidel m-0">
+          <Middle />
+        </section>
+        <section className="hidel m-0 bg-[#ba45b4]">
+          <Bottom />
+        </section>
+      </div>
     </div>
   );
 };
