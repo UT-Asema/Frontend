8c46eedb80d9bccf3507143260c6e073b6bae02c
erupturatis <bb.octav@gmail.com>
2022-12-10 11:39:42 +0200

    setupd3

--- src/Pages/Roadmap/Roadmap.jsx
+++ src/Pages/Roadmap/Roadmap.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import * as d3 from "d3";
 
 const ShowRoadmap = () => {
-  return <div>takes id of the roadmap</div>;
+  useEffect(() => {
+    d3.select("body").attr("class", "overflow-hidden");
+    return () => {
+      d3.select("body").attr("class", "");
+    };
+  }, []);
+
+  return (
+    <div className="border-2 h-full w-full flex">
+      <div className="w-64 h-full border-2">Menu bar</div>
+      <div className="w-full h-full">
+        <svg
+          id="root-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+        >
+          <g id="root-group"></g>
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default ShowRoadmap;
