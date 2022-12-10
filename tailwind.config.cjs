c4785ba050e3ac1e4fbbb96866bb76d4bab41916
Arking <rusalexandru115@gmail.com>
2022-12-10 10:24:47 +0200

    (template) Created Templated For Login

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "split-accent-white": "linear-gradient(to right, #ba45b4 50%, white 50%)"
+      }
+    },
   },
   plugins: [],
 };
